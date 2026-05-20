3e0f303d2997f9ff9aa597133b9194606afb9ba0
ultimatefirechamp <rlacksrb1121@gmail.com>
2026-05-20 15:02:43 +0900

    [AIController] 적 인공지능 행동에 대한 지침

--- Assets/Scripts/Practice/Character/AIController.cs
+++ Assets/Scripts/Practice/Character/AIController.cs
@@ -24,6 +24,77 @@ public class AIController : MonoBehaviour
         BattleManager.Inst.RegistEnemy(_controllerable as CharacterScript);
     }
 
+    public void AITurn()
+    {
+        if(_controllerable == null)
+        {
+            return;
+        }
+
+        if (_controllerable is CharacterScript bot == false)
+        {
+            return;
+        }
+        // 현재 체력이 20% 이하 OR 사지절단 상태라면 -> 자폭
+        // 미구현
+        // 플레이어가 사정거리 안에 있다면 -> 공격
+
+        Vector2Int myPos = bot.GridPosition;
+        Vector2Int playerPos = MapManager.Inst.WorldToArrayPos(playerTransform.position);
+        int deltaX = Mathf.Abs(myPos.x - playerPos.x);
+        int deltaY = Mathf.Abs(myPos.y - playerPos.y);
+        int distance = deltaX > deltaY ? deltaX : deltaY;
+        if(distance <= bot.AttackRange)
+        {
+            _controllerable.Attack(playerPos);
+            return;
+        }
+        // 플레이어를 향해 이동.
+        FindPath(myPos, playerPos);
+    }
+    void FindPath(Vector2Int myPos, Vector2Int playerPos)
+    {
+        List<Vector2Int> path = MapManager.Inst.GetPathToTarget(myPos, playerPos);
+        if (path == null)
+        {
+            Debug.LogWarning("NULL path Error");
+            return;
+        }
+
+        if (path.Count == 0) // There is no path to player
+        {
+            return;
+        }
+        Debug.LogWarning($"First path : {path[0]}");
+        Vector2Int direction = path[0] - myPos;
+        Vector2Int destPos = path[0];
+
+        if (MapManager.Inst.IsOccupied(destPos) == false && MapManager.Inst.IsWalkable(destPos))
+        {
+            _controllerable.Move(direction);
+            return;
+        } // Can move that dir
+
+        // Cant move that dir, check adjacent direction
+        var (leftDirection, rightDirection) = MyUtil.GetAdjacentDirections(direction);
+
+        // left dir check
+        destPos = myPos + leftDirection;
+        if (MapManager.Inst.IsOccupied(destPos) == false && MapManager.Inst.IsWalkable(destPos))
+        {
+            _controllerable.Move(leftDirection);
+            return;
+        }
+
+        // right dir check
+        destPos = myPos + rightDirection;
+        if (MapManager.Inst.IsOccupied(destPos) == false && MapManager.Inst.IsWalkable(destPos))
+        {
+            _controllerable.Move(rightDirection);
+            return;
+        }
+    }
+
     public void OnTurn()
     {
         Vector2Int myPos = MapManager.Inst.WorldToArrayPos(transform.position);
--- Assets/Scripts/Practice/Manager/BattleManager.cs
+++ Assets/Scripts/Practice/Manager/BattleManager.cs
@@ -9,7 +9,18 @@ public class BattleManager : MonoBehaviour
     private List<CharacterScript> _enemyList;
     private GameObject _enemyPrefab;
     public bool IsPlayerTurn { get; private set; }
+    
+    bool IsMyTurn(CharacterScript script)
+    {
+        if(script.CompareTag("Player") == IsPlayerTurn)
+        {
+            return true;
+        }
+        return false;
+    }
+    
     private static BattleManager _instance;
+    
     private void Awake()
     {
         if(_instance == null)
@@ -19,17 +30,22 @@ public class BattleManager : MonoBehaviour
         IsPlayerTurn = true;
         _enemyList = new List<CharacterScript>();
     }
+    
     private void Start()
     {
+        // 디버깅용으로 편의상 로드해둔 데이터들
         _enemyPrefab = PracticeResourceManager.Inst.LoadPrefab("Prefabs/Practice_KCK/Character/Enemy");
+        // 실제 인게임 상에서 배틀 매니저가 enemy 스폰에 직접적 관여 금지
         if(GameDataManager.Instance.GetSkill("skill_smash") == null)
         {
             Debug.LogWarning("Skill Data NULL");
         }
-
+        // 스킬도 확인 용으로 임시로 받은 데이터.
         Debug.Log((GameDataManager.Instance.GetSkill("skill_smash")).Name);
     }
+    
     public static BattleManager Inst { get { return _instance; } }
+    
     public void RegistEnemy(CharacterScript enemy)
     {
         _enemyList.Add(enemy);
@@ -54,20 +70,23 @@ public class BattleManager : MonoBehaviour
             targetCharacter.TakeDamage(1);
         }
     }
+
     public void RequestSkill(CharacterScript caster, Vector2Int target, Skill skill)
     {
         // skill check and execute Skill
-        if(IsPlayerTurn == false)
+        if(IsMyTurn(caster) == false)
         {
             return;
         }
+
         skill.Execute(caster,target);
     }
+
     private void Update()
     {
         if(IsPlayerTurn == false)
         {
-            if(Input.anyKeyDown)
+            if (Input.anyKeyDown)
             {
                 EnemyTurn();
                 Input.ResetInputAxes();
@@ -86,11 +105,11 @@ public class BattleManager : MonoBehaviour
     {
         foreach(var enemy in _enemyList)
         {
-            
             var controller = enemy.gameObject.GetComponent<AIController>();
-            controller.OnTurn();
+            controller.AITurn();
         }
     }
+
     public void TurnChange()
     {
         IsPlayerTurn = !IsPlayerTurn;
@@ -98,12 +117,14 @@ public class BattleManager : MonoBehaviour
         mainUI.GetComponent<PracticeMainUI>().SetCurrentTurn(IsPlayerTurn);
         Debug.Log($"Turn Changed : {IsPlayerTurn}");
     }
+
     public void SpawnEnemy()
     {
         GameObject spawnedEnemy = Instantiate(_enemyPrefab);
         spawnedEnemy.transform.position = new Vector2(0.5f, 0.5f);
         _enemyList.Add(spawnedEnemy.GetComponent<CharacterScript>());
     }
+
     public CharacterScript GetSpawnEnemy()
     {
         GameObject spawnedEnemy = Instantiate(_enemyPrefab);
