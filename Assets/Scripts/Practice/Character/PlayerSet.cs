957e4ebeaf1be68998bb85c384f88beeed76f7f7
ultimatefirechamp <rlacksrb1121@gmail.com>
2026-05-21 15:56:19 +0900

    [Player] 플레이어 캐릭터 정보를 세팅하는 컴포넌트 추가

---
+++ Assets/Scripts/Practice/Character/PlayerSet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+public class PlayerSet : MonoBehaviour
+{
+    MonsterData playerData = new MonsterData();
+    CharacterScript _player;
+    bool _isSet = false;
+    private void Start()
+    {
+        playerData.HP = 20;
+        playerData.AC = 1;
+        playerData.Range = 1;
+        playerData.ATK = 3;
+        if (gameObject.TryGetComponent<CharacterScript>(out _player) == false)
+        {
+            return;
+        }
+        _isSet = true;
+    }
+}
