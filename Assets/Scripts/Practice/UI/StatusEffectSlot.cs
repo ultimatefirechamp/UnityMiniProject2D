45fc1fd4a14f1569276098cb15252f61f67822aa
ultimatefirechamp <rlacksrb1121@gmail.com>
2026-05-22 19:22:28 +0900

    [UI] 상태이상 표시 UI추가

---
+++ Assets/Scripts/Practice/UI/StatusEffectSlot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusEffectSlot : MonoBehaviour
+{
+    [SerializeField] Image _spriteImg;
+    [SerializeField] Text _stackCount;
+    [SerializeField] Text _durationCount;
+
+    public void RefreshSlot(StatusEffect effect)
+    {
+        _stackCount.text = $"{effect.Stack}";
+        _durationCount.text = $"{effect.Duration}";
+    }
+    public void SetSlotSprite(Sprite sprite)
+    {
+        _spriteImg.sprite = sprite;
+    }
+}
---
+++ Assets/Scripts/Practice/UI/StatusUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class StatusUI : MonoBehaviour
+{
+
+    public void RegisterOwner(CharacterScript _onwer)
+    {
+
+    }
+
+    void MoveUI(Transform transform)
+    {
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        rectTransform.position = screenPos;
+    }
+
+}
