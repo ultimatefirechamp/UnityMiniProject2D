11dbf66add79e6a4d37815e2892ba1d83cdccea4
ultimatefirechamp <rlacksrb1121@gmail.com>
2026-05-26 11:12:39 +0900

    [Debug] 디버깅용 인풋 커맨드를 적을 때 쓸 코드 추가.

---
+++ Assets/Scripts/Practice/UI/DebugInputField.cs
@@ -0,0 +1,34 @@
+﻿using AutoGroupGenerator;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugInputField : MonoBehaviour
+{
+    [SerializeField] InputField _commandInputField;
+    
+    void ProcessCommand(string input)
+    {
+        string[] args = input.Split(' ');
+        string command = args[0].ToLower();
+
+        switch(command)
+        {
+            case "spawn":
+                break;
+            case "addeffect":
+                break;
+        }
+    }
+    void SpawnEnemy()
+    {
+
+    }
+    void AddEffect()
+    {
+
+    }
+    void MovePosition()
+    {
+
+    }
+}
