8c5b4c903ced8298f9585dc8e45c4395f546a4f0
ultimatefirechamp <rlacksrb1121@gmail.com>
2026-05-26 11:43:02 +0900

    [EnemySpawner] 적 개체를 소환할 오브젝트

---
+++ Assets/Scripts/Practice/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyPrefab;
+
+    public void SpawnEnemy(Vector2Int gridPosition)
+    {
+        if(MapManager.Inst.IsOccupied(gridPosition))
+        {
+            return; 
+        }
+        GameObject createdEnemyObj = Instantiate(_enemyPrefab);
+        createdEnemyObj.transform.position = MapManager.Inst.ArrayToWorldPos(gridPosition.x, gridPosition.y);
+        CharacterScript createdEnemy = createdEnemyObj.GetComponent<CharacterScript>();
+        createdEnemy.SetGridPosition(gridPosition);
+        MapManager.Inst.OccupyTile(gridPosition, createdEnemy);
+    }
+
+}
