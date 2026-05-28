d4e4ca3745a8477015ff3207fe770a24a53086e8
ultimatefirechamp <rlacksrb1121@gmail.com>
2026-05-28 15:19:22 +0900

    [Character] 캐릭터 구조 순수 C#클래스로 변경하며 Trait 클래스 새로 생김, 기존 Component라는 명칭이 붙은 클래스 -> System으로 변경

--- Assets/Scripts/Practice/Skill/SkillSystem.cs
+++ Assets/Scripts/Practice/Skill/SkillSystem.cs
@@ -1,34 +1,25 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterScript))]
-public class SkillComponent : MonoBehaviour
+public class SkillSystem
 {
     Dictionary<string, SkillRecord> _skillList;
     Dictionary<SkillComboType, string> _skillMaping;
     CharacterScript _owner;
-    bool _isSet = false;
 
-    private void Awake()
+
+    public SkillSystem(CharacterScript owner)
     {
-        if(gameObject.TryGetComponent<CharacterScript>(out _owner) == false)
-        {
-            return;
-        }
-        _isSet = true;
+        _owner = owner;
         _skillList = new Dictionary<string, SkillRecord>();
         _skillMaping = new Dictionary<SkillComboType, string>();
-    }
-    
-    private void Start()
-    {
-        AddSkill("skill_flyingswallow",SkillComboType.SHIFT);
+        AddSkill("skill_flyingswallow", SkillComboType.SHIFT);
         AddSkill("skill_walljump");
     }
 
     public void UseSkill(string skillId, Vector2Int target)
     {
-        if (_skillList.TryGetValue(skillId, out var skill) == false || _isSet == false)
+        if (_skillList.TryGetValue(skillId, out var skill) == false)
         {
             return;
         }
@@ -41,11 +32,6 @@ public class SkillComponent : MonoBehaviour
     }
     public void UseSkill(SkillComboType comboType, Vector2Int target)
     {
-        if(_isSet == false)
-        {
-            Debug.LogWarning("Can't Find CharacterScript!");
-            return;
-        }
         if(_skillMaping.TryGetValue(comboType, out var skillId))
         {
             UseSkill(skillId, target);
@@ -53,10 +39,6 @@ public class SkillComponent : MonoBehaviour
     }
     public void AddSkill(string skillId, SkillComboType comboType = SkillComboType.NONE)
     {
-        if(_isSet == false)
-        {
-            return;
-        }
         _skillList.Add(skillId, GameDataManager.Instance.GetSkillRecord(skillId));
         if(comboType == SkillComboType.NONE)
         {
---
+++ Assets/Scripts/Practice/Skill/TraitData.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class TraitData
+{
+    
+}
+
+public abstract class Trait
+{
+    protected CharacterScript _owner;
+    public virtual void Equip(CharacterScript owner)
+    {
+        _owner = owner;
+    }
+    public virtual void UnEquip()
+    {
+        _owner = null;
+    }
+}
+
+// 그냥 데이터 드리븐으로 얘도 조건과 함께 뺄 수 있을지도 모르지만...
+// Ex. 조건:효과:양
+// 일단 규모가 커지고 그때 중복되는게 많아지는 거 같으면 다듬는 걸로... 트레잇 1,2개 밖에 없고 중복되는 기능도 없으면
+// 조합 / 데이터 드리븐을 해봤자 의미가 별로 없는거 같음...
+// 또 특성/스킬마다 각자 유니크한 걸 만드려고 하면
+// 조합이나 재활용 할 만한 구석이 안나오는 거 같음
+
+public class HealOnKillTrait : Trait
+{
+    int spHealAmount = 4;
+    int hpHealAmount = 5;
+
+    public override void Equip(CharacterScript owner)
+    {
+        base.Equip(owner);
+        _owner.OnKillEvent += HandleKill;
+    }
+    public override void UnEquip()
+    {
+        if (_owner != null)
+        {
+            _owner.OnKillEvent -= HandleKill;
+        }
+        base.UnEquip();
+    }
+    public void HandleKill(CharacterScript owner)
+    {
+        _owner.Heal(hpHealAmount);
+
+    }
+}
