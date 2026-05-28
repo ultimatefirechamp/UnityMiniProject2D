70bd7c963fbe6ad48f0e8ec037e6d10af193051d
ultimatefirechamp <rlacksrb1121@gmail.com>
2026-05-28 20:11:50 +0900

    [Other] 일단 임시로 인게임씬을 다루는 스크립트 적용

---
+++ Assets/Scripts/Practice/IngameScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class IngameScene : MonoBehaviour
+{
+    [SerializeField] Tilemap wallTile;
+    private void Start()
+    {
+        MapManager.Inst.SetWallTileMap(wallTile);
+    }
+}
