3847b6f4b878b1607a5b6d5c84c33898799ea1a4
ultimatefirechamp <rlacksrb1121@gmail.com>
2026-05-28 15:22:23 +0900

    [PlayerSpawnPoint] 플레이어 소환 포인트 개념 추가.

---
+++ Assets/Scripts/Practice/PlayerSpawnPoint.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerSpawnPoint : MonoBehaviour
+{
+    [SerializeField] Transform RootTransform;
+    private void Start()
+    {
+        SpawnPlayer();
+    }
+    public void SpawnPlayer()
+    {
+        Vector2Int position = MapManager.Inst.WorldToArrayPos(this.transform.position);
+        var playerObject = ObjectManager.Inst.SpawnUnit(position);
+        playerObject.transform.SetParent(RootTransform);
+        playerObject.AddComponent<PlayerController>();
+        var player = playerObject.GetComponent<CharacterScript>();
+
+        BattleManager.Inst._player = player;
+
+        MonsterData playerData = new MonsterData();
+        playerData.HP = 20;
+        playerData.AC = 1;
+        playerData.Range = 1;
+        playerData.ATK = 3;
+        player.SetCharacter(playerData);
+
+        HUDLayout hud;
+        hud = PracticeUIManager.Inst.GetCreatedUI(UIType.HUD).GetComponent<HUDLayout>();
+        hud.RegistPlayer(player);
+    }
+}
