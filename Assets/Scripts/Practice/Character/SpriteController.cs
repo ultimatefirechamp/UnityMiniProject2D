3fbe374bbd5b0a204a8210f1810e8ceec1d20134
ultimatefirechamp <rlacksrb1121@gmail.com>
2026-05-28 15:25:14 +0900

    [SpriteController] 캐릭터 스프라이트를 관리할 스프라이트 컨트롤러 추가

---
+++ Assets/Scripts/Practice/Character/SpriteController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteController : MonoBehaviour
+{
+    [SerializeField] List<Sprite> _spriteList;
+    [SerializeField] SpriteRenderer _spriteRenderer;
+    
+
+}
